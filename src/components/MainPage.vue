eddbc56b38282c5b337a8c850c5cbe3293d93929
3guna3 <83042016+3guna3@users.noreply.github.com>
2021-12-16 12:11:32 +0900

    mainを修正

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="main-page">
-    メインページです
+    <div class="left-menu">
+      左メニュー
+    </div>
+    <div class="right-view">
+      右ビュー
+    </div>
   </div>
+
 </template>
 
 <script>
@@ -10,8 +16,17 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .main-page {
-  padding: 10px;
+  display: flex;
+  height: calc(100vh - 60px);
+  .left-menu {
+    width: 350px;
+    background-color: #f7f6f3;
+  }
+  .right-view {
+    flex-grow: 1;
+    padding: 10px;
+  }
 }
 </style>
